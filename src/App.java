35467f9d11ff0aee46310e178f07930ae0f45e26
AlexanderLM <144786514+AlexanderTheGreat69z@users.noreply.github.com>
2024-05-30 01:39:08 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,8 +1,110 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        int row = 2, col = 2;
-        ArrayMatrix arrMat = new ArrayMatrix(row, col);
 
+    public static void testArrayMatrix(int row, int col) {
+
+        // Generate random entries for Array Matrix //
+        ArrayMatrix arrMat = new ArrayMatrix(row, col);
+        System.out.println("Generated Matrix:");
+        arrMat.generateRandomMatrix();
+        arrMat.display();
+
+        // DETERMINANT TEST: Calculate the determinant of the matrix //
+        long a_start = System.currentTimeMillis();
+
+        System.out.println("\nDeterminant of above matrix:");
+        System.out.println(arrMat.determinant());
+
+        long a_end = System.currentTimeMillis();
+
+        // ROW REDUCTION TEST: Row reduce the matrix into Row Echelon Form //
+        long b_start = System.currentTimeMillis();
+
+        ArrayMatrix reduced = arrMat.reduce();
+        System.out.println("\nRow reduced version:");
+        reduced.display();
+
+        long b_end = System.currentTimeMillis();
+
+        // MATRIX MULTIPLICATION TEST: Calculate the product between two matrices //
+        long c_start = System.currentTimeMillis();
+
+        ArrayMatrix A = new ArrayMatrix(row, col);
+        System.out.println("\nlet A = ");
+        A.generateRandomMatrix();
+        A.display();
+
+        ArrayMatrix B = new ArrayMatrix(row, col);
+        System.out.println("\nlet B = ");
+        B.generateRandomMatrix();
+        B.display();
+
+        System.out.println("\nA . B =");
+        A.matrixMultiply(B).display();
+
+        long c_end = System.currentTimeMillis();
+
+
+        System.out.println("\nArrayMatrix executions:");
+        System.out.println("- Determinant\t: " + (a_end - a_start) + " ms");
+        System.out.println("- Reduction\t: " + (b_end - b_start) + " ms");
+        System.out.println("- Multiply\t: " + (c_end - c_start) + " ms");
+    }
+
+    public static void testLinkedMatrix(int row, int col){
+        long start = System.currentTimeMillis();
+
+        // Generate random entries for Array Matrix //
+        LinkedMatrix linkMat = new LinkedMatrix(row, col);
+        System.out.println("Generated Matrix:");
+        linkMat.generateRandomMatrix();
+        linkMat.display();
+
+        // DETERMINANT TEST: Calculate the determinant of the matrix //
+        long a_start = System.currentTimeMillis();
+
+        System.out.println("\nDeterminant of above matrix:");
+        System.out.println(linkMat.determinant());
+        
+        long a_end = System.currentTimeMillis();
+
+        // ROW REDUCTION TEST: Row reduce the matrix into Row Echelon Form //
+        long b_start = System.currentTimeMillis();
+
+        LinkedMatrix reduced = linkMat.reduce();
+        System.out.println("\nRow reduced version:");
+        reduced.display();
+
+        long b_end = System.currentTimeMillis();
+
+        // MATRIX MULTIPLICATION TEST: Calculate the product between two matrices //
+        long c_start = System.currentTimeMillis();
+
+        ArrayMatrix A = new ArrayMatrix(row, col);
+        System.out.println("\nlet A = ");
+        A.generateRandomMatrix();
+        A.display();
+
+        ArrayMatrix B = new ArrayMatrix(row, col);
+        System.out.println("\nlet B = ");
+        B.generateRandomMatrix();
+        B.display();
+
+        System.out.println("\nA . B =");
+        A.matrixMultiply(B).display();
+
+        long c_end = System.currentTimeMillis();
+
+        System.out.println("\nLinkedMatrix executions:");
+        System.out.println("- Determinant\t: " + (a_end - a_start) + " ms");
+        System.out.println("- Reduction\t: " + (b_end - b_start) + " ms");
+        System.out.println("- Multiply\t: " + (c_end - c_start) + " ms");
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        int row = 10, col = 10;
+
+        // Set matrix entries //
         // float[][] values = {
         //     {3, 0, 2, -1},
         //     {1, 2, 0, 4},
@@ -12,46 +114,13 @@ public class App {
 
         // for (int i = 0; i < row; i++) {
         //     for (int j = 0; j < col; j++) {
-        //         arrMat.setEntry(i+1, j+1, values[i][j]);
+        //         linkMat.setEntry(i+1, j+1, values[i][j]);
         //     }
         // }
 
-        // Randomly generate entries for matrix //
-        System.out.println("Randomly Generated Matrix:");
-        arrMat.generateRandomMatrix();
-        arrMat.display();
+        testArrayMatrix(row, col);
+        System.out.println("\n----------------------------------------------------------------------------------------\n");
+        testLinkedMatrix(row,col);
 
-        // ROW REDUCTION TEST: Row reduce the matrix into Row Echelon Form //
-        System.out.println("\nRow reduced version:");
-        arrMat.reduce();
-        arrMat.display();
-
-        // DETERMINANT TEST: Calculate the determinant of the matrix //
-        System.out.println("\nDeterminant of above matrix:");
-        System.out.println(arrMat.determinant());
-
-        // MATRIX MULTIPLICATION TEST: Calculate the product between two matrices //
-        ArrayMatrix A = new ArrayMatrix(row, col);
-        ArrayMatrix B = new ArrayMatrix(row, col);
-
-        float[][] val_a = {
-            {2, 3},
-            {4, 1},
-        };
-
-        float[][] val_b = {
-            {5, 6},
-            {7, 8},
-        };
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                A.setEntry(i+1, j+1, val_a[i][j]);
-                B.setEntry(i+1, j+1, val_b[i][j]);
-            }
-        }
-
-        System.out.println("\nA . B =");
-        A.matrixMultiply(B).display();
     }
 }
