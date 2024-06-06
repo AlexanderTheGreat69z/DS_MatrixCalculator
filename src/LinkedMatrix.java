4de2d4c1f739825d7971808245117df884b9f7b4
AlexanderLM <144786514+AlexanderTheGreat69z@users.noreply.github.com>
2024-06-06 09:42:49 +0700

    Update LinkedMatrix.java

--- src/LinkedMatrix.java
+++ src/LinkedMatrix.java
@@ -1,11 +1,10 @@
 import java.util.LinkedList;
 
-public class LinkedMatrix {
-    int row, col;
+public class LinkedMatrix extends Matrix{
     LinkedList<LinkedList<Float>> matrix;
+    
     LinkedMatrix(int row, int col){
-        this.row = row;
-        this.col = col;
+        super(row, col);
         this.matrix = new LinkedList<>();
 
         for(int r = 0; r < row; r++){
@@ -45,13 +44,13 @@ public class LinkedMatrix {
     //     C = A;
     // }
 
-    private void addEntry(int row, int col, float add){
+    public void addEntry(int row, int col, float add){
         LinkedList<Float> matRow = this.matrix.get(row-1);
         matRow.set(col-1, matRow.get(col-1) + add);
     }
 
-    private LinkedMatrix copy(){
-        LinkedMatrix matCopy = new LinkedMatrix(this.row, this.col);
+    public Matrix copy(){
+        Matrix matCopy = new LinkedMatrix(this.row, this.col);
         for (int r = 1 ; r <= this.row ; r++){
             for (int c = 1 ; c <= this.col; c++){
                 matCopy.setEntry(r, c, this.getEntry(r, c));
@@ -60,17 +59,6 @@ public class LinkedMatrix {
         return matCopy;
     }
 
-    public void display(){
-        for(int r = 0; r < this.row; r++){
-            System.out.print("[ ");
-            for(int c = 0; c < this.col; c++){
-                if (c > 0){System.out.print("  ");}
-                System.out.printf("%.2f", this.getEntry(r+1, c+1));
-            }
-            System.out.print(" ]");
-            System.out.println();
-        }
-    }
 
     public void generateRandomMatrix(){
         for (int r = 1; r <= this.row; r++){
@@ -81,8 +69,8 @@ public class LinkedMatrix {
         }
     }
 
-    public LinkedMatrix minorMatrix(int mr, int mc){
-        LinkedMatrix minorMat = new LinkedMatrix(this.row - 1, this.col - 1);
+    public Matrix minorMatrix(int mr, int mc){
+        Matrix minorMat = new LinkedMatrix(this.row - 1, this.col - 1);
         int row = 1;
         int col = 1;
         for (int r = 1; r <= this.row; r++){
@@ -108,113 +96,9 @@ public class LinkedMatrix {
 
 
     public float cofactor(int mr, int mc){
-        LinkedMatrix cofactorMat = this.minorMatrix(mr, mc);
+        Matrix cofactorMat = this.minorMatrix(mr, mc);
         float det = cofactorMat.determinant();
         float control = (float)(Math.pow(-1, mr+mc));
         return control * det;
-    }
-
-    public int getRowSize(){return this.row;}
-    public int getColSize(){return this.col;}
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    
-    public LinkedMatrix reduce(){
-        LinkedMatrix reduced = this.copy();
-        // int lastSlot = this.row;
-
-        for (int c = 1 ; c <= this.col; c++){
-
-            for (int r = c + 1; r <= this.row; r++){
-                
-                float pivot = reduced.getEntry(c, c);
-                float scale = reduced.getEntry(r, c);
-
-                if (pivot != 0){
-                    for(int i = 1; i <= this.col; i++){
-                        float new_entry = reduced.getEntry(r, i) - (scale/pivot) * reduced.getEntry(c, i);
-                        reduced.setEntry(r, i, new_entry);
-                    }
-                }
-            }
-        }
-
-        // for (int i = 0; i < this.row; i++){
-        //     if(this.isZeroRow(i)){
-        //         this.interchange(i, lastSlot-1);
-        //         lastSlot--;
-        //     }
-        // }
-        return reduced;
-    }
-
-    public LinkedMatrix matrixMultiply(ArrayMatrix b){
-        if(this.col == b.getRowSize()){
-            int prod_row = this.row;
-            int prod_col = b.getColSize();
-
-            LinkedMatrix product = new LinkedMatrix(prod_row, prod_col);
-            for(int i = 1; i <= prod_row; i++){
-
-                for(int j = 1; j <= prod_col; j++){
-
-                    for(int k = 1; k <= this.col; k++){
-
-                        float A = this.getEntry(i, k);
-                        float B = b.getEntry(k, j);
-                        product.addEntry(i, j, A*B);
-                        
-                    }
-                }
-            }
-            return product;
-        }
-        else{
-            return null;
-        }
-    }
-
-    public float determinant(){
-        if(this.row == this.col){
-            if (this.row == 1){
-                return this.getEntry(1, 1);
-            }
-            if (this.row == 2){
-                float a = this.getEntry(1, 1) * this.getEntry(2, 2);
-                float b = this.getEntry(2, 1) * this.getEntry(1, 2);
-                return a - b;
-            }
-            if (this.row == 3){
-                float aei = this.getEntry(1, 1) * this.getEntry(2,2) * this.getEntry(3, 3);
-                float bfg = this.getEntry(1, 2) * this.getEntry(2,3) * this.getEntry(3, 1);
-                float cdh = this.getEntry(1, 3) * this.getEntry(2,1) * this.getEntry(3, 2);
-
-                float ceg = this.getEntry(1, 3) * this.getEntry(2,2) * this.getEntry(3, 1); 
-                float bdi = this.getEntry(1, 2) * this.getEntry(2,1) * this.getEntry(3, 3);
-                float afh = this.getEntry(1, 1) * this.getEntry(2,3) * this.getEntry(3, 2);
-
-                float x = aei + bfg + cdh;
-                float y = ceg + bdi + afh;
-
-                return x - y;
-            }
-
-            if (this.row > 3){
-                // ArrayMatrix reduced = this.reduce();
-                // int det = 1;
-
-                // for (int i = 1; i <= this.row; i++){
-                //     det *= reduced.getEntry(i, i);
-                // }
-
-                int defaultRow = 1;
-                float det = 0;
-                for (int i = 1; i <= this.col; i++){
-                    det += this.getEntry(defaultRow, i) * this.cofactor(defaultRow, i);
-                }
-                return det;
-            }
-        }
-        return 0;
     }
 }
